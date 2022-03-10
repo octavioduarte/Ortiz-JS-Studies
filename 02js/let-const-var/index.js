03e7c73cd798731a0327b2050d02a9b841ea066b
octavioduarte <octavioduarte2105@gmail.com>
2022-03-09 22:35:30 -0300

    instructions about var let and const

---
+++ 02js/let-const-var/index.js
@@ -0,0 +1,25 @@
+/**
+ * Para criação de variaveis em JavaScript usamos 3 palavras reservadas
+ */
+
+
+
+// const >> Não pode ter o valor alterado
+const userDocument = "48482918191"
+
+
+//-------------------------------------------------------------------------------------
+
+        // Var e let podem ter valores alterados - Diferença entre ambas é o escopo
+
+let userEmail = "user@mail.com"
+userEmail = "user@mail2.com"
+
+
+//-------------------------------------------------------------------------------------
+
+var userVarEmail = "userVar@mail.com"
+userVarEmail = "userVar@mail2.com"
+
+
+//-------------------------------------------------------------------------------------
