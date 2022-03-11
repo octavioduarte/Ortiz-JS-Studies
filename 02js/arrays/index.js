69f713368a6ef4450524caac845cee3947ce87c8
octavioduarte <octavioduarte2105@gmail.com>
2022-03-10 21:20:01 -0300

    add file with instructions about array concepts

---
+++ 02js/arrays/index.js
@@ -0,0 +1,17 @@
+
+// Tipos primitivos de variaveis
+
+const nameUser = "Octávio"
+const age = 22
+const brazilian = true 
+const bornDate = new Date()
+
+const nullType = null
+
+
+// Array >> "Armazenar multiplas informações dentro de uma unica variavel" 
+
+const arrayType = [ nameUser, age, brazilian, bornDate ]
+
+
+console.log(arrayType)
