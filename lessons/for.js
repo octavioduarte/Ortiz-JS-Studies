029626a8297a748385b75e8ca72bc78cfc8a378c
octavioduarte <octavioduarte2105@gmail.com>
2022-03-10 22:22:41 -0300

    create first lesson about for

---
+++ lessons/for.js
@@ -0,0 +1,12 @@
+const arrayNumbers = [1, 2, 3, 4, 5]
+
+/**
+ *  Monte um for que percorra arrayNumbers e imprima com o console.log
+ * o dobro de cada um dos valores.
+ * 
+ * Dica: Multiplicação em JS se usa o asterisco, exemplo: 10 * 3 = 30
+ *  
+ * */ 
+
+
+// Digite sua resposta na linha abaixo
