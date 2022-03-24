012b3698c6fea91543e94ed9f6aede9a88a3125b
octavioduarte <octavioduarte2105@gmail.com>
2022-03-23 21:01:33 -0300

    basic example about use of if

---
+++ 02js/if/index.js
@@ -0,0 +1,16 @@
+
+// Simulando dados do Banco de Dados
+const emailDB = "octavio@email.com"
+const passwordDB = 1234
+
+
+
+const emailForm = "octavio@emai3l.com"
+const passwordForm = 1234
+
+
+if (emailDB === emailForm && passwordDB === passwordForm) {
+    console.log("Credenciais corretas")
+} else {
+    console.log("Email ou senha incorretos")
+}
